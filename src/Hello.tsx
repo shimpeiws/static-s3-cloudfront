9f222c5c4e5aa88ade6b484d0a8690e3c625014d
Shimpei Takamatsu <shimpeiws@gmail.com>
2020-05-12 15:49:43 +0900

    Initial commit

---
+++ src/Hello.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export interface Props {}
+
+export function Hello(_: Props) {
+  return <div>Hello</div>;
+}
---
+++ webpack.config.dev.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/Index.tsx",
+  output: {
+    filename: "./js/bundle.js"
+  },
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [{ loader: "ts-loader" }]
+      }
+    ]
+  },
+
+  devServer: {
+    open: true,
+    contentBase: path.join(__dirname, "./dist"),
+    watchContentBase: true,
+    port: 3000
+  }
+};
